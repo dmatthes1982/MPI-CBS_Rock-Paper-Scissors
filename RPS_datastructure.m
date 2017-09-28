7717507c0840b23339598aacae6b17d85181d40c
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-09-28 16:12:13 +0200

    help file – RPS_DATASTRUCTURE - updated

--- RPS_datastructure.m
+++ RPS_datastructure.m
@@ -1,21 +1,19 @@
 % RPS_DATASTRUCTURE
 %
-% The data in the --- Joint Attention Imitation Projekt --- ist structured 
-% as follows:
+% The data in the --- Rock, Paper, Scissor Projekt --- ist structured as 
+% follows:
 %
 % Every step of the data processing pipeline (i.e. 01_raw, 02_preproc, 
-% 03_tfr1 ...) generates N single datasets, where N describes the current 
-% number of dyads within the study. Every dataset for each dyad is stored 
-% in a separate *.mat file, to avoid the need of swap memory during data 
-% processing. A datasets has always two fields, which are named part1 and 
-% part2. Each field comprises a 1x1 struct with the complete data of a 
-% specific participant. The different conditions in this data struct are 
-% separated through trials and the field trialinfo contains the condition 
-% markers of each trials. In case of subsegmented data the structure 
-% contains more than one trial for each condition. If no trial was rejected 
-% during the preprocessing, there should be 36 trials per condition in the 
-% data structure. The information about the order of the trials of one 
-% condition is available through the relating time elements. 
+% 03_tfr1 ...) produces N single *.mat files, where N describes the 
+% current number of dyads within the study. Each *.mat file holds a 'data'
+% struct consisting of two fields, which are named part1 and part2. Each 
+% field again comprises a 1x1 struct with the complete data (data of all 
+% four conditions) of a specific participant. The data ist stored for each 
+% dyad separately, to avoid the need of swap memory during data processing.
+% The data itself is structured in trials. Every trial is defined as a
+% specific phase in a specific condition. A corresponding number is stored 
+% in the field trialinfo of the participants data struct. The order of the 
+% trials in one data struct is available through the relating time field.
 %
 % dataset example:
 %
@@ -24,23 +22,27 @@
 %    |---- part1 (1x1 fieldtrip data structure for participant 1)    
 %    |---- part2 (1x1 fieldtrip data structure for participant 2)
 %   
+% Many functions, especially the plot functions, need a specification of 
+% the phase and condition, which should be selected. Currently the 
+% following specifications are existent:
 %
-% Many functions especially the plot functions need a definition of the 
-% specific condition, which should be selected. Currently the following 
-% conditions are existent:
+% - FreePlay_Prediction   - 111
+% - FreePlay_ButtonPress  - 112
+% - FreePlay_Action       - 113
+% - FreePlay_PanelDown    - 114
+% - PredDiff_Prediction   - 211
+% - PredDiff_ButtonPress  - 212
+% - PredDiff_Action       - 213
+% - PredDiff_PanelDown    - 214
+% - PredSame_Prediction   - 311
+% - PredSame_ButtonPress  - 312
+% - PredSame_Action       - 313
+% - PredSame_PanelDown    - 314
+% - Control_Prediction    - 411
+% - Control_ButtonPress   - 412
+% - Control_Action        - 413
 %
-% - ViewMotion        - 2
-% - SameMotion        - 3
-% - ConImi12          - 31
-% - ConImi21          - 32
-% - ConOthAct12       - 41
-% - ConOthAct21       - 42
-% - SponImiI          - 51
-% - SponImiII         - 52
-% - Conversation      - 105
-% - SameObject        - 111
-%
-% The defintion of the condition is done by setting the cfg.condition
-% option with the string or the number of the specific condition.
+% The defintion of the condition is done by setting the cfg.state
+% option with the string or the number of the specific state.
 
 % Copyright (C) 2017, Daniel Matthes, MPI CBS
