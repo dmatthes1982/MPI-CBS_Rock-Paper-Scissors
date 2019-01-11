8ef1053ee82940860217614c65f151b60cc26fdf
Daniel Matthes <dmatthes@cbs.mpg.de>
2019-01-11 10:22:16 +0100

    Function RPS_databrowser – opacity of artifact elements increased by using the new config option cfg.artifactalpha of ft_databrowser → opacity value changed from 0.2 to 0.7

--- RPS_eeg_pipeline/functions/RPS_databrowser.m
+++ RPS_eeg_pipeline/functions/RPS_databrowser.m
@@ -82,6 +82,7 @@ cfg.continuous                    = 'no';
 cfg.channel                       = channel;
 cfg.plotevents                    = plotevents;
 cfg.event                         = event;
+cfg.artifactalpha                 = 0.7;
 cfg.showcallinfo                  = 'no';
 
 fprintf('Databrowser - Condition: %d - Participant: %d\n', cond, part);
