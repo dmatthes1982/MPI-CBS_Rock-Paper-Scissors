0fde5fa5148226b5d91e98ca750916df631964b8
Daniel Matthes <dmatthes@cbs.mpg.de>
2019-03-28 11:54:12 +0100

    Script RPS_permtest_plv – NEW script → does permutation test by using the paired-sample t-test as test statistic. → The permutation is done on PLV level → all single PLV segments of both conditions are randomly permuted within one dyad. (null hypothesis – no difference between conditions) Script RPS_permtest_mplv – some comments changed → reference documentation added → condition acronym will also be added to output data structure

--- RPS_eeg_pipeline/stats/RPS_permtest_plv.m
+++ RPS_eeg_pipeline/stats/RPS_permtest_plv.m
@@ -1,16 +1,14 @@
-% RPS_PERMTEST_MPLV - Using this script one can test if the difference of
+% RPS_PERMTEST_PLV - Using this script one can test if the difference of
 % one connection (e.g. Cz-Cz) or one cluster of connections is significant
-% between two phases. First, a paired-sample t-test is conducted. If
-% this test shows a significant result, a permutation test follows. Here,
-% the permutation is done on the level of averaged PLV values. Since our
-% null hypothesis says that we expect no difference between phases but
-% a difference between dyads, the permutation is done only by randomly
-% interchanging the values of the two phases within the dyads. It is
-% highly recommended to only use this script, if the number of PLV segments
-% is nearly the same in both phases for single dyads. Otherwise one
-% should use RPS_PERMTEST_PLV
-%
-% See also RPS_PERMTEST_PLV
+% between two phases. First, a paired-sample t-test on the level of
+% averaged PLV values is conducted. If this test shows a significant
+% result, a permutation test follows. Here, the permutation is done on the
+% level of single PLV values. All values from both phases of one dyad
+% are collected in a single set. Afterwards the phases will be
+% recovered by randomly drawing PLV values from the combined set.
+% The number of drawn PLV values per phase for each permutation will
+% always be the same and will be kept identically with the original
+% distribution.
 
 % -------------------------------------------------------------------------
 % Add directory and subfolders to path, clear workspace, clear command
@@ -21,7 +19,7 @@ run([filepath '/../RPS_init.m']);
 
 cprintf([0,0.6,0], '<strong>-------------------------------------------</strong>\n');
 cprintf([0,0.6,0], '<strong>Rock paper scissors project</strong>\n');
-cprintf([0,0.6,0], '<strong>Permutation test on mplv level</strong>\n');
+cprintf([0,0.6,0], '<strong>Permutation test on plv level</strong>\n');
 cprintf([0,0.6,0], 'Copyright (C) 2019, Daniel Matthes, MPI CBS\n');
 cprintf([0,0.6,0], '<strong>-------------------------------------------</strong>\n');
 
@@ -59,9 +57,9 @@ clear newPaths
 % -------------------------------------------------------------------------
 fprintf('\n<strong>Session selection...</strong>\n');
 srcPath = [datastorepath 'DualEEG_RPS_processedData/'];
-srcPath = [srcPath  '07b_mplv/'];
+srcPath = [srcPath  '07a_plv/'];
 
-fileList     = dir([srcPath, 'RPS_d*_07b_mplvAlpha_*.mat']);
+fileList     = dir([srcPath, 'RPS_d*_07a_plvAlpha_*.mat']);
 fileList     = struct2cell(fileList);
 fileList     = fileList(1,:);
 numOfFiles   = length(fileList);
@@ -70,7 +68,7 @@ sessionNum   = zeros(1, numOfFiles);
 fileListCopy = fileList;
 
 for dyad=1:1:numOfFiles
-  fileListCopy{dyad} = strsplit(fileList{dyad}, '07b_mplvAlpha_');
+  fileListCopy{dyad} = strsplit(fileList{dyad}, '07a_plvAlpha_');
   fileListCopy{dyad} = fileListCopy{dyad}{end};
   sessionNum(dyad) = sscanf(fileListCopy{dyad}, '%d.mat');
 end
@@ -135,7 +133,7 @@ fprintf('You have selected the following passband: %s\n\n', passband);
 % Dyad selection
 % -------------------------------------------------------------------------
 fprintf('<strong>Dyad selection...</strong>\n');
-fileList     = dir([srcPath 'RPS_d*_07b_mplv' passband '_' sessionStr ...
+fileList     = dir([srcPath 'RPS_d*_07a_plv' passband '_' sessionStr ...
                     '.mat']);
 fileList     = struct2cell(fileList);
 fileList     = fileList(1,:);                                               % generate list with filenames of all existing dyads
@@ -243,7 +241,7 @@ fprintf(['If you are selecting multiple connections, the selection '...
 
 load([srcPath fileList{1}]);                                                % load data of first dyad
 
-label     = data_mplv.FP.dyad.label;                                        % extract channel names
+label     = data_plv.FP.dyad.label;                                         % extract channel names
 numOfChan = length(label);
 
 label_x = repmat(label, 1, numOfChan);                                      % prepare a cell array with all possible connections for cluster specification
@@ -266,12 +264,13 @@ for i=1:1:length(row)
 end
 
 connections = connMatrix(connMatrixBool);
+numOfConn   = numel(connections);
 
 fprintf('\nYou have selected the following connections:\n');
 cellfun(@(x) fprintf('%s, ', x), connections, 'UniformOutput', false);      % show the identifiers of the selected connections in the command window
 fprintf('\b\b.\n\n');
               
-clear data_mplv numOfChan connMatrix row col part i label_x label_y ...
+clear data_plv numOfChan connMatrix row col part i label_x label_y ...
       selection x prompt_string
 
 % -------------------------------------------------------------------------
@@ -280,81 +279,91 @@ clear data_mplv numOfChan connMatrix row col part i label_x label_y ...
 fprintf('<strong>Import of PLV values...</strong>\n');
 f = waitbar(0,'Please wait...');
 
-cnt               = 0;
+cnt                    = 0;
 data_stat.goodDyadsNum = NaN(numOfFiles, 1);
 data_stat.condAcronym  = condAcronym;
 data_stat.trialinfo    = NaN(numOfFiles * 2, 1);
-data_stat.mPLV         = NaN(1, numOfFiles * 2);
+data_stat.mPLV         = [];
+PLV.data = cell(numOfFiles, numOfConn);
 
 for dyad = 1:1:numOfFiles
   waitbar(dyad/numOfFiles, f, sprintf('Please wait %d/%d...', dyad, ...
           numOfFiles));
   load([srcPath fileList{dyad}]);                                           % load data
-  
-  if any(~strcmp(data_mplv.FP.dyad.label, label))
+
+  if any(~strcmp(data_plv.FP.dyad.label, label))
     error(['Error with dyad %d. The channels are not in the correct ' ...
             'order!\n'], dyads(dyad));
   end
 
   if dyad == 1                                                              % extract bandpass specification
     data_stat.passband    = passband;
-    data_stat.range       = data_mplv.bpFreq;
+    data_stat.range       = data_plv.bpFreq;
     data_stat.connections = connections;
   end
   
-  tmptrialinfo = [  data_mplv.FP.dyad.trialinfo; ...
-                    data_mplv.PD.dyad.trialinfo + 20; ...
-                    data_mplv.PS.dyad.trialinfo + 40; ...
-                    data_mplv.C.dyad.trialinfo + 60 ];
+  tmptrialinfo = [  data_plv.FP.dyad.trialinfo; ...
+                    data_plv.PD.dyad.trialinfo + 20; ...
+                    data_plv.PS.dyad.trialinfo + 40; ...
+                    data_plv.C.dyad.trialinfo + 60 ];
   tf = ismember(tmptrialinfo, phaseNum);                                    % check if selected phases are exisiting
   if(sum(tf) ~=2)
     cprintf([1,0.5,0], sprintf(['At least one phase is missing. ' ...
-                      'Dyad %d will not be considered.\n'], dyads(dyad))); 
+                       'Dyad %d will not be considered.\n'], dyads(dyad)));
   else                                                                      % extract PLV values
-    cnt      = cnt + 1;
-    mPLVtemp = cell(1,2);
+    cnt = cnt + 1;
     phaseNumOrg = zeros(1,2);
     for phase=1:2
       if phaseNum(phase) <= 20                                              % trial from condition FP
-        tf = ismember(data_mplv.FP.dyad.trialinfo, phaseNum(phase));
-        mPLVtemp(phase) = data_mplv.FP.mPLV(tf);
+        tf = ismember(data_plv.FP.dyad.trialinfo, phaseNum(phase));
+        PLV.data(cnt, phase) = data_plv.FP.PLV(tf);
       elseif (20 < phaseNum(phase)) && (phaseNum(phase) <=40 )              % trial from condition PD
         phaseNumOrg(phase) = phaseNum(phase) - 20;
-        tf = ismember(data_mplv.PD.dyad.trialinfo, phaseNumOrg(phase));
-        mPLVtemp(phase) = data_mplv.PD.dyad.mPLV(tf);
+        tf = ismember(data_plv.PD.dyad.trialinfo, phaseNumOrg(phase));
+        PLV.data(cnt, phase) = data_plv.PD.dyad.PLV(tf);
       elseif (40 < phaseNum(phase)) && (phaseNum(phase) <=60 )              % trial from condition PS
         phaseNumOrg(phase) = phaseNum(phase) - 40;
-        tf = ismember(data_mplv.PS.dyad.trialinfo, phaseNumOrg(phase));
-        mPLVtemp(phase) = data_mplv.PS.dyad.mPLV(tf);
+        tf = ismember(data_plv.PS.dyad.trialinfo, phaseNumOrg(phase));
+        PLV.data(cnt, phase) = data_plv.PS.dyad.PLV(tf);
       elseif phaseNum(phase) > 60                                           % trial from condition C
         phaseNumOrg(phase) = phaseNum(phase) - 60;
-        tf = ismember(data_mplv.C.dyad.trialinfo, phaseNumOrg(phase));
-        mPLVtemp(phase) = data_mplv.C.dyad.mPLV(tf);
+        tf = ismember(data_plv.C.dyad.trialinfo, phaseNumOrg(phase));
+        PLV.data(cnt, phase) = data_plv.C.dyad.PLV(tf);
       end
     end
-    mPLVtemp  = cellfun(@(x) x(connMatrixBool), ...
-                       mPLVtemp, 'UniformOutput', false);
-    mPLVtemp  = cellfun(@(x) mean(x), ...                                   % average over connections
-                       mPLVtemp, 'UniformOutput', false);
-    
-    data_stat.mPLV(2*cnt-1:2*cnt)      = cell2mat(mPLVtemp);
+    PLV.data(cnt, :)  = cellfun(@(x) x(connMatrixBool), ...
+                       PLV.data(cnt, :), 'UniformOutput', false);
     data_stat.trialinfo(2*cnt-1:2*cnt) = phaseNumOrg;
     data_stat.goodDyadsNum(cnt)        = dyads(dyad);
   end
-  clear data_mplv
+  clear data_plv
 end
 
 close(f);
 
+datatmp = cell(cnt, numel(phaseNum), numOfConn);                            % transform nested cell structure into an unnested 3-dimensional cell structure
+for row = 1:cnt
+  for col = 1:numel(phaseNum)
+    datatmp(row, col, 1:numOfConn) = PLV.data{row, col};
+  end
+end
+PLV.data = datatmp;
+
+mPLVtemp = cellfun(@(x) mean(x), PLV.data, 'UniformOutput', false);         % estimate averaged PLV values (averaging over phases)
+mPLVtemp = cell2mat(mPLVtemp);
+mPLVtemp = mean(mPLVtemp, 3);
+mPLVtemp = reshape(mPLVtemp', 1, []);
+
+data_stat.mPLV         = mPLVtemp;                                          % add averaged PLV values to the output structure
 data_stat.goodDyadsNum = data_stat.goodDyadsNum(1:cnt);
 data_stat.trialinfo    = data_stat.trialinfo(1:2*cnt);
-data_stat.mPLV         = data_stat.mPLV(1:2*cnt);
 numOfGoodDyads         = numel(data_stat.goodDyadsNum);
 
 fprintf('\n');
 
 clear f cnt mPLVtemp tf dyad connections connMatrixBool dyads fileList ...
-      passband label numOfFiles phase tmptrialinfo phaseNum condAcronym
+      passband label numOfFiles datatmp row col condAcronym tmptrialinfo...
+      phase
 
 % -------------------------------------------------------------------------
 % Run t-Test
@@ -371,6 +380,7 @@ data_stat.stat.tstat  = stats.tstat;
 data_stat.stat.df     = stats.df;
 data_stat.stat.sd     = stats.sd;
 
+
 if data_stat.stat.p < 0.05                                                  % check if result is significant
   fprintf('The t-test result is significant: %s=%g\n\n', ...
           char(945), data_stat.stat.p);
@@ -378,7 +388,7 @@ else
   fprintf('The t-test result is NOT significant: %s=%g\n', ...
           char(945), data_stat.stat.p);
   fprintf('Skip permutation test...\n\n');
-  clear phase1 phase2 h p ci stats phaseNumOrg datastorepath ...
+  clear phase1 phase2 h p ci stats phaseNum datastorepath ...
         numOfGoodDyads sessionStr srcPath
   return                                                                    % return if result is non-significant
 end
@@ -390,47 +400,67 @@ clear phase1 phase2 h p ci stats
 % -------------------------------------------------------------------------
 fprintf('<strong>Run permutation test...</strong>\n');
 
-design(:,1) = 1:2:2*numOfGoodDyads;                                         % specify permutation design
-design(:,2) = 2:2:2*numOfGoodDyads;
-design = mat2cell(design,ones(1,numOfGoodDyads), 2);
-design = design';
+PLV.numel = cellfun(@(x) numel(x), PLV.data(:,:,1), 'UniformOutput', false);
 
-numOfPerm = 2500;                                                           % specify number of permutations (500 more than required)
-resample = zeros(numOfPerm, 2*numOfGoodDyads);
-hasDuplicates    = true;
-
-fprintf('Generate permutation matrix...\n');
-while hasDuplicates
-  for i=1:numOfPerm
-    for j=1:numOfGoodDyads
-      resample(i, design{j}) = design{j}(randperm(2));
-    end
-  end
-
-  [u, loc] = unique(resample, 'rows', 'first');                             % test for duplicates
-  hasDuplicates = size(u,1) < size(resample,1);
-  if(hasDuplicates)
-    loc = sort(loc);
-    resample = resample(loc, :);
-    if(size(resample,1) > numOfPerm - 500)
-      numOfPerm = numOfPerm - 500;
-      resample  = resample(1:numOfPerm,:);
-      hasDuplicates = false;
-    end
-  else
-    numOfPerm = numOfPerm - 500;
-    resample  = resample(1:numOfPerm, :);
+for row = 1:numOfGoodDyads                                                  % concatenate data of both phases within a dyad
+  for z = 1:numOfConn
+    PLV.data{row, 1, z} = [PLV.data{row,:,z}];
   end
 end
+PLV.data(:, 2, :) = [];
+PLV.data = squeeze(PLV.data);
+
+numOfPerm = 2500;                                                           % specify number of permutations (500 more than required)
+PLV.resample = cellfun(@(x) zeros(numOfPerm, numel(x)), PLV.data(:,1,1),...
+                          'UniformOutput', false);
+
+fprintf('Generate permutation matrix...\n');
+
+for i = 1:numOfGoodDyads                                                    % generate seperate permutation matricies for all dyads
+  hasDuplicates    = true;
+  while hasDuplicates
+    for j = 1:numOfPerm
+      PLV.resample{i}(j,:) = randperm(size(PLV.resample{i}, 2));
+    end
+    
+    [u, loc] = unique(PLV.resample{i}, 'rows', 'first');                    % test for duplicates
+    hasDuplicates = size(u,1) < size(PLV.resample{i}, 1);
+    if(hasDuplicates)
+      loc = sort(loc);
+      PLV.resample{i} = PLV.resample{i}(loc, :);
+      if(size(PLV.resample{i}, 1) > numOfPerm - 500)
+        PLV.resample{i}  = PLV.resample{i}(1:numOfPerm - 500,:);
+        hasDuplicates = false;
+      end
+    else
+      PLV.resample{i}  = PLV.resample{i}(1:numOfPerm - 500, :);
+    end
+  end
+end
+
+numOfPerm = numOfPerm - 500;                                                % reduce number of permutations after cleaning the permutation matrix from duplicates
 
 fprintf('Run test...\n');
 data_stat.tstatPerm = zeros(1, numOfPerm);
 
-for i=1:1:numOfPerm
-  tmptrialinfo = data_stat.trialinfo(resample(i,:));
-  phase1 = ismember(tmptrialinfo, phaseNumOrg(1));
-  phase2 = ismember(tmptrialinfo, phaseNumOrg(2));
-  [~,~,~,stats] = ttest(data_stat.mPLV(phase1),data_stat.mPLV(phase2));
+for i=1:numOfPerm
+  mPLVtemp = cell(numOfGoodDyads, numel(phaseNum), numOfConn);              % estimate averaged PLV values using permuted data
+  for row = 1:numOfGoodDyads
+    for z = 1:numOfConn
+      mPLVtemp{row, 1, z} = PLV.data{row,z}(PLV.resample{row}...
+                                              (i, 1:PLV.numel{row,1}));
+      mPLVtemp{row, 2, z} = PLV.data{row,z}(PLV.resample{row}...
+                                              (i, PLV.numel{row,1}+1:end));
+    end
+  end
+  mPLVtemp = cellfun(@(x) mean(x), mPLVtemp, 'UniformOutput', false);
+  mPLVtemp = cell2mat(mPLVtemp);
+  mPLVtemp = mean(mPLVtemp, 3);
+  mPLVtemp = reshape(mPLVtemp', 1, []);
+  
+  phase1 = ismember(data_stat.trialinfo, phaseNumOrg(1));
+  phase2 = ismember(data_stat.trialinfo, phaseNumOrg(2));
+  [~,~,~,stats] = ttest(mPLVtemp(phase1), mPLVtemp(phase2));                % run paired-sample t-test
   data_stat.tstatPerm(i) = stats.tstat;
 end
 
@@ -440,8 +470,8 @@ data_stat.pPerm = sum(abs(data_stat.tstatPerm) > ...
                       abs(data_stat.stat.tstat)) / numOfPerm;
 data_stat.numOfPerm = numOfPerm;
 
-clear design numOfPerm resample hasDuplicates i j u loc phase1 phase2 ...
-      tmptrialinfo stats phaseNumOrg numOfGoodDyads
+clear mPLVtemp numOfPerm resample hasDuplicates i j u loc phase1 phase2 ...
+      stats phaseNumOrg phaseNum numOfGoodDyads row z numOfConn PLV
 
 % -------------------------------------------------------------------------
 % Test result
@@ -475,7 +505,7 @@ while selection == false
     cprintf([1,0.5,0], ['Use only letters and or numbers for the file '...
                         'identifier\n']);
   else
-    matFile = [desPath 'RPS_mplvStats_' identifier{1} '_' sessionStr ...    % build filename
+    matFile = [desPath 'RPS_plvStats_' identifier{1} '_' sessionStr ...     % build filename
                 '.mat'];
 
     if exist(matFile, 'file')                                               % check if file already exists
